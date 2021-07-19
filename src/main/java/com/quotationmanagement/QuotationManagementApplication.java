f8badbbafdfa39893da8006cff9814587b475b3e
Wagner Paulino <wagnersantos9778@gmail.com>
2021-07-19 08:29:10 -0300

    initial commit

---
+++ src/main/java/com/quotationmanagement/QuotationManagementApplication.java
@@ -0,0 +1,23 @@
+package com.quotationmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableCaching
+public class QuotationManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotationManagementApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+}
